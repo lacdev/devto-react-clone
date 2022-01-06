280e8ad2962a6078c626ceea3b4e78eabef9842f
cmtzvitela <cmtzvitela@gmail.com>
2022-01-05 22:41:06 -0600

    Sticker side done and Listings on the making

---
+++ src/components/Listings.js
@@ -0,0 +1,22 @@
+<div class="bg-gray-200 w-60 rounded">
+  <div class="flex justify-around py-2">
+    <h2 class="font-bold">Listings</h2>
+    <span class="text-blue-500 ">See all</span>
+  </div>
+  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
+    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
+    <a href="#" class="text-xs">collabs</a>
+  </div>
+  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
+    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
+    <a href="#" class="text-xs">collabs</a>
+  </div>
+  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
+    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
+    <a href="#" class="text-xs">collabs</a>
+  </div>
+  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
+    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
+    <a href="#" class="text-xs">collabs</a>
+  </div>
+</div>
