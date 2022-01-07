0d2072f99b1a9f6a5b47214a65f0cd6c4f2dcd6d
cmtzvitela <cmtzvitela@gmail.com>
2022-01-06 20:42:17 -0600

    Added listings sidebar

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,22 +1,39 @@
-<div class="bg-gray-200 w-60 rounded">
-  <div class="flex justify-around py-2">
-    <h2 class="font-bold">Listings</h2>
-    <span class="text-blue-500 ">See all</span>
+import React from 'react'
+
+function Listings() {
+    return (
+        <div>
+            <div class="bg-[#f9f9f9] text-[#3d3d3d] w-60 ">
+  <div class="flex justify-around py-2 rounded">
+    <h2 class="font-bold pr-32">Listings</h2>
+    <span class="text-blue-500 text-xs text-right pt-1">See all</span>
   </div>
-  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
-    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
+  <div class="p-3 text-sm border-2 hover:bg-white hover:text-[#323ebe]">
+    <a href= "#"class="">Noted links - Curated resources for developers, designers and web makers</a> <br/>
     <a href="#" class="text-xs">collabs</a>
   </div>
-  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
-    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
+  <div class="p-3 text-sm border-2 hover:bg-white hover:text-[#323ebe]">
+    <a href= "#"class="">Easy money with your raspberry pi</a> <br/>
+    <a href="#" class="text-xs">products</a>
+  </div>
+  <div class="p-3 text-sm border-2 hover:bg-white hover:text-[#323ebe]">
+    <a href= "#"class="">Master Visual study book</a> <br/>
+    <a href="#" class="text-xs">forsale</a>
+  </div>
+  <div class="p-3 text-sm border-2 hover:bg-white hover:text-[#323ebe]">
+    <a href= "#"class="">We need an Android app (GUI-) developer - FOSS project licensed GPL v2</a> <br/>
     <a href="#" class="text-xs">collabs</a>
   </div>
-  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
-    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
-    <a href="#" class="text-xs">collabs</a>
+  <div class="p-3 text-sm border-2 hover:bg-white hover:text-[#323ebe]">
+    <a href= "#"class="">TheJam.dev - 2 Day Jamstack Virtual Conference - Jan 26-27 - FREE</a> <br/>
+    <a href="#" class="text-xs">events</a>
   </div>
-  <div class="p-3 text-sm border-2 border-black border-solid hover:bg-white">
-    <a href= "#"class="hover:text-blue-500">Noted links - Curated resources for developers, designers and web makers</a> <br>
-    <a href="#" class="text-xs">collabs</a>
+  <div class="flex justify-around py-2 rounded hover:text-[#323ebe]">
+    <a href= "#" class="text-xs text-right pt-1">Create a listing</a>
   </div>
 </div>
+        </div>
+    )
+}
+
+export default Listings
