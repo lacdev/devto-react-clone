51868b1ce8618898993d5a7c205a71f9e2fe0a1b
jramon901225 <jramon901225@gmail.com>
2022-01-06 22:57:17 -0500

    SearchBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { MiComponent } from "./components/ResultsSideBar";
 import { ReadNext } from "./components/ReadNext";
+import { SearchBar } from "./components/SearchBar";
 
 const App = () => {
   return (
     <>
-      <ReadNext />
+      <SearchBar />
     </>
   );
 };
