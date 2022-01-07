2cf0f7b597ca557a3dd3b4c4cf8fcb558fb737a1
S3len3Ch4vz <nasaya.369@gmail.com>
2022-01-06 21:18:36 -0600

    Inicio de Router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import { Header } from 'components/Header'
-import { SubHeader } from 'components/Subheader'
-import { SubSubHeader } from 'components/Subsubheader'
-import { Button } from 'components/Button'
-import { Title } from 'components/Title'
-import { Container } from 'components/Container'
-import { IconList } from 'components/IconList'
-import { OtherList } from 'components/OtherList'
-import { SocialMediaIcons } from 'components/SocialMediaIcons'
-import {Footer} from 'components/Footer'
-import { OnePostRightBar } from 'components/OnePostRightBar'
-import { Navbar } from 'components/Navbar'
+import{BrowserRouter, Route, Routes} from 'react-router-dom';
+
+//Components
+
+
+
+//Pages
+import NotFoundPage from 'pages/NotFoundPage'
+import Home from 'pages/Home'
+import Post from 'pages/Post';
+import Search from 'pages/Search';
+import CreatePost from 'pages/CreatePost';
 
 
 const App = () => {
   return (
-    <div className="bg-black">
-       <Navbar/>
-       <OnePostRightBar/>
-      <Container>
-      <Header>This is our Dev.to Clone </Header>
-      <SubHeader> Less Cool Text </SubHeader>
-      <SubSubHeader> Kind of boring text </SubSubHeader>
-      <Title> Hello World </Title>
-      <Button> I'm a cool button </Button>
-     
-      <IconList/>
-      <OtherList/>
-      <SocialMediaIcons/>
-     
-      </Container>
-      <Footer/>
-    </div>
+  
+    <BrowserRouter>
+    <Routes>
+    <Route path="*" element={<NotFoundPage/>}></Route>
+    <Route path="/" element={<Home/>}> </Route>
+    <Route path="/Post" element={<Post/>}></Route>
+    <Route path="/Search" element={<Search/>}></Route>
+    <Route path="/CreatePost" element={<CreatePost/>}></Route>
+    
+    </Routes>
+    </BrowserRouter>
+   
   )
 }
 
