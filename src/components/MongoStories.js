44170fafaede948f6b36ad34e5bc080f57dc5959
German <german.mtz@protonmail.com>
2022-01-06 21:35:24 -0600

    add MongoStories Component

---
+++ src/components/MongoStories.js
@@ -0,0 +1,21 @@
+const MongoStories = () => {
+  return (
+    <>
+      <div className="container rounded-lg flex-col mx-auto">
+        <img
+          className="rounder-lg"
+          src="https://dev-to-uploads.s3.amazonaws.com/uploads/articles/kkxvvytot7mztccmawdb.png"
+          alt="mongodb banner"
+        ></img>
+
+        <div className="flex-col"></div>
+        <div className="flex-col p-4">
+          <button>Submit your project!</button>
+          <button>See All Posts</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export { MongoStories }
