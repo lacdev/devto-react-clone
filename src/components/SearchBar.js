c0634acc1c642c044c074a70d2cf1cc582d1bc7d
jramon901225 <jramon901225@gmail.com>
2022-01-10 23:30:56 -0500

    cards on search page stylized

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { getPosts } from "services/posts";
+import { Loader } from "./Loader";
 
 //CSS properties
 const cssInput =
@@ -8,14 +10,15 @@ const cssIcon =
   " absolute -translate-x-[30px]  translate-y-[15px] cursor-pointer ";
 const form = "relative display-inline";
 
+//Onkeydown
 export const SearchBar = () => {
   const navigate = useNavigate("");
   const [searchTerm, setSearchTerm] = useState("");
 
   const onSubmitForm = (event) => {
+   //Prevenimos la recarga del navegador
     event.preventDefault();
     if (searchTerm === "") return;
-
     //Navegamos a la pagina de resultados
     navigate({
       pathname: "/search",
@@ -43,6 +46,7 @@ export const SearchBar = () => {
           aria-hidden="true"
           class="crayons-icon c-btn__icon"
           focusable="false"
+          onClick={onSubmitForm}
         >
           <path d="m18.031 16.617 4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z"></path>
         </svg>
--- src/components/SearchPageCard.js
+++ src/components/SearchPageCard.js
@@ -81,6 +81,7 @@ export const SearchPageCard = () => {
                     src={post?.avatar}
                     alt="linus"
                   ></img>
+
                   <div>
                     <h1 className="text-3xl font-bold mx-3 mt-3">
                       {" "}
@@ -93,11 +94,13 @@ export const SearchPageCard = () => {
                         post?.date
                       )})`}
                     </h2>
+
                     <QueryNavLink to={`/post/${post?._id}`}>
                       <h1 className="font-bold text-3xl mt-3 mb-3 mx-3 cursor-pointer hover:text-indigo-700">
                         {post?.title}
                       </h1>
                     </QueryNavLink>
+
                     <div className="flex">
                       {post?.tags?.map((tag) => {
                         return (
@@ -117,6 +120,7 @@ export const SearchPageCard = () => {
                         >
                           <path d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z"></path>
                         </svg>
+
                         {`${post?.reactions} Reactions`}
                       </span>
                     </button>
